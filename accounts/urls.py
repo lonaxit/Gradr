5e3f07d6e4aa388cbe469924a61733d8c83fc522
lonaxit <lonaxit@outlook.com>
2021-05-20 16:40:47 +0100

    Changed url patterns

--- accounts/urls.py
+++ accounts/urls.py
@@ -3,15 +3,15 @@ from . import views
 
 
 urlpatterns = [
-  path('accounts/register', views.register, name='register'),
-  path('accounts/login', views.loginPage, name='login'),
-  path('accounts/logout', views.logoutUser, name='logout'),
-  path('accounts/contact', views.contact, name='contact'),
+  path('register', views.register, name='register'),
+  path('login', views.loginPage, name='login'),
+  path('logout', views.logoutUser, name='logout'),
+  path('contact', views.contact, name='contact'),
 
-  path('accounts/profile', views.profile, name='profile'),
-  path('accounts/staff', views.staff),
-  path('accounts/student', views.profile),
-  path('accounts/student-list', views.studentList),
+  path('profile', views.profile, name='profile'),
+  path('staff', views.staff),
+  path('student', views.profile),
+  path('student-list', views.studentList),
   path('admin-home', views.admin, name='admin'),
   path('users', views.users, name='users'),
 ]
