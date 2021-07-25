45c55d612157822e1158ffe5edd6ca1819c9f46b
lonaxit <lonaxit@outlook.com>
2021-07-25 10:17:17 +0100

    Created new urls

--- teacher/urls.py
+++ teacher/urls.py
@@ -8,7 +8,8 @@ urlpatterns = [
     # path('country-json', views.get_json_country_data, name='country-json'),
   path('class-subjects/<str:pk>', views.get_subjects, name='class-subjects'),
   path('filter-scores', views.scoresFilter, name='filter-scores'),
-  path('update-scores/<str:pk>', views.editScores, name='update-scores'),
+  path('update-scores/<int:id>', views.editScores, name='update-scores'),
+  path('remove-scores/<int:id>', views.deleteScores, name='delete-scores'),
   # path('lg-json/<str:pk>', views.get_json_lg_data, name='lg-json'),
   # path('update-profile/<str:pk>', views.updateClient, name='update-profile'),
 
