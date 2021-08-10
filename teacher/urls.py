f3227df5e238c51797370c6ef42f483272bc7ec5
lonaxit <lonaxit@outlook.com>
2021-08-10 12:28:11 +0100

    Added result urls

--- teacher/urls.py
+++ teacher/urls.py
@@ -10,6 +10,8 @@ urlpatterns = [
   path('filter-scores', views.scoresFilter, name='filter-scores'),
   path('update-scores/<int:id>', views.editScores, name='update-scores'),
   path('remove-scores/<int:id>', views.deleteScores, name='delete-scores'),
+  path('filter-result', views.resultFilter, name='filter-result'),
+  path('add-comment', views.addComment, name='add-comment'),
   # path('lg-json/<str:pk>', views.get_json_lg_data, name='lg-json'),
   # path('update-profile/<str:pk>', views.updateClient, name='update-profile'),
 
