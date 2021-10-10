9eeba5f047a476f27dfde37c7786f26614d15448
lonaxit <lonaxit@outlook.com>
2021-10-10 22:08:32 +0100

    Updated settings for heroku deployment

--- gradr/settings.py
+++ gradr/settings.py
@@ -12,7 +12,10 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os
-
+# imports for deployment
+import django_heroku
+import dj_database_url
+from decouple import config
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -53,6 +56,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    # configure white noise for deployment
+    'whitenoise.middleware.WhiteNoiseMiddleware'
 ]
 
 ROOT_URLCONF = 'gradr.urls'
@@ -133,6 +138,11 @@ STATICFILES_DIRS=[
     os.path.join(BASE_DIR,'gradr/static')
 ]
 
+# configure static files for heroku deployment
+
+STATICFILES_STORAGE ='whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+
 
 # configure media root for storing images
 # MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
@@ -146,3 +156,6 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# configure for deployement on heroku
+django_heroku.settings(locals())
