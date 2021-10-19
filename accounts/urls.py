cfca51f515788f39235987079316ade6b1fa5c5f
lonaxit <lonaxit@outlook.com>
2021-10-19 14:58:59 +0100

    Removed unused urls

--- accounts/urls.py
+++ accounts/urls.py
@@ -6,14 +6,5 @@ urlpatterns = [
   path('register', views.register, name='register'),
   path('login', views.loginPage, name='login'),
   path('logout', views.logoutUser, name='logout'),
-
-
-
-  path('contact', views.contact, name='contact'),
-  path('profile', views.profile, name='profile'),
-  path('staff', views.staff),
-  path('student', views.profile),
-  path('student-list', views.studentList),
   path('admin-home', views.admin, name='admin'),
-  path('users', views.users, name='users'),
 ]
