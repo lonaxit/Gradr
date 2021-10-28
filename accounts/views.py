eeceb71eb2efce4a496d23a127f5c01799913b38
lonaxit <lonaxit@outlook.com>
2021-10-28 14:00:49 +0100

    Removed comment

--- accounts/views.py
+++ accounts/views.py
@@ -110,7 +110,7 @@ def logoutUser(request):
 @login_required(login_url='login')
 @admin_only
 def admin(request):
-    #  nocommentsCount = result.filter(classteachercomment__isnull=True).count()
+   
     students = Student.objects.filter(reg_no__isnull=True)
     context = {'students':students}
     return render(request,'accounts/admin.html',context)
