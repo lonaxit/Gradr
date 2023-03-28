9b60068775e5adafe974b9513a51fd32f0049fe5
lonaxit <lonaxit@outlook.com>
2023-03-28 18:09:13 +0100

    Corrected a  bug

--- teacher/views.py
+++ teacher/views.py
@@ -729,7 +729,7 @@ def myClassroom(request,classroom,term,session):
         
         # classTeacher = ClassTeacher.objects.get(teacher=loggedin,term=termObj,session=sessObj)
     #    new code
-        stds = Classroom.objects.filter(Q(session=sessObj.pk) & Q(class_room=stdClass.pk)).order_by('student__sur_name')
+        stds = Classroom.objects.filter(Q(session=sessObj.pk) & Q(class_room=stdClass.pk) & Q(term=termObj.pk)).order_by('student__sur_name')
         
         # old
         # stds = Classroom.objects.filter(Q(term=termObj.pk) & Q(session=sessObj.pk) & Q(class_room=stdClass.pk)).order_by('student__sur_name')
